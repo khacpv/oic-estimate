5cffe734f81b0262c058b01fa92a4c6dbf75599f
khacpham <khacpv@gmail.com>
2017-03-07 18:31:15 +0700

    add project setting

--- 2-structured-data/config.js
+++ 2-structured-data/config.js
@@ -42,7 +42,7 @@ nconf
     DATA_BACKEND: 'datastore',
 
     // This is the id of your project in the Google Cloud Developers Console.
-    GCLOUD_PROJECT: 'oic-estimate',
+    GCLOUD_PROJECT: '119249617253',
 
     // MongoDB connection string
     // https://docs.mongodb.org/manual/reference/connection-string/
